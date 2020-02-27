e3c3b8a0c10c6609f3a4d0bfaf808611a0bb98e3
Joe Granville <metaregress@gmail.com>
2020-02-26 19:32:21 -0800

    Adding pathfinding

--- Assets/BasicEnemy.cs
+++ Assets/BasicEnemy.cs
@@ -1,15 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class BasicEnemy : Enemy
 {
+
+    private Seeker seeker;
+    public GameObject player;
+    public float speed;
+    private Path path;
+
+    private float nextWaypointDistance = 3;
+
+    private int currentWaypoint = 0;
+
+    private bool reachedEndOfPath;
+
+    private Vector3 targetLastPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = 20;
         currentHealth = maxHealth;
         contactDamage = 1;
+        seeker = GetComponent<Seeker>();
+        seeker.pathCallback = OnPathComplete;
+
+        targetLastPosition = player.transform.position;
+        
+
+        seeker.StartPath(transform.position, targetLastPosition);
     }
 
     // Update is called once per frame
@@ -20,6 +42,53 @@ public class BasicEnemy : Enemy
             Destroy(gameObject);
         }
 
+        DoMovement();        
+
+        // simple AI, no pathfinding yet
+        // it seems like a popular pathfinding algorithm is called A*
+        // but it will require us to have a pathfinding grid before we can do it
+
+
+    }
+
+    void DoMovement()
+    {
+        if(targetLastPosition != player.transform.position)
+        {
+            targetLastPosition = player.transform.position;
+            seeker.StartPath(transform.position, targetLastPosition);
+        }
+        if (path == null)
+        {
+            return;
+        }
+
+        reachedEndOfPath = false;
+        float distanceToWaypoint;
+        while (true)
+        {
+            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+            if(distanceToWaypoint < nextWaypointDistance)
+            {
+                if(currentWaypoint + 1 < path.vectorPath.Count)
+                {
+                    currentWaypoint++;
+                }
+                else
+                {
+                    reachedEndOfPath = true;
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        Vector3 velocity = dir * speed;
+        transform.position += velocity * Time.deltaTime;
     }
 
     public override int GetContactDamage()
@@ -35,5 +104,22 @@ public class BasicEnemy : Enemy
         {
             player.currentHealth -= GetContactDamage();
         }
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Yay, we got a path back. Did it have an error? " + p.error);
+
+        if (!p.error)
+        {
+            path = p;
+            // Reset the waypoint counter so that we start to move towards the first point in the path
+            currentWaypoint = 0;
+        }
+    }
+
+    public void OnDisable()
+    {
+        seeker.pathCallback -= OnPathComplete;
     }
 }
