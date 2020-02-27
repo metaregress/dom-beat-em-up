652f61956bffbfc00decc03f798656573424a250
Joe Granville <metaregress@gmail.com>
2020-02-26 19:56:27 -0800

    Redoing move API to avoid copypasta

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour
+public abstract class Move : MonoBehaviour
 {
     public string moveName;
 
@@ -12,15 +12,38 @@ public class Move : MonoBehaviour
 
     public AttackStateMachine stateMachine;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    public int startup;
+    public int active;
+    public int recovery;    
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-
+        if (stateMachine.timeLeftInState <= 0)
+        {
+            // We've finished our startup frames, time to go active!
+            if (stateMachine.currentState == AttackStateMachine.States.Startup)
+            {
+                stateMachine.SetNewState(AttackStateMachine.States.Active, active);
+                OnActive();
+            }
+            // We've finished our active frames, time to go into recovery
+            else if (stateMachine.currentState == AttackStateMachine.States.Active)
+            {
+                stateMachine.SetNewState(AttackStateMachine.States.Recovery, recovery);
+                OnRecovery();
+            }
+            // If we're done recovering, shut down the state machine in a None state to indicate the move isn't in use
+            else if (stateMachine.currentState == AttackStateMachine.States.Recovery)
+            {
+                stateMachine.SetNewState(AttackStateMachine.States.None, 1);
+                stateMachine.runTimer = false;
+            }
+        }
     }
+
+    protected abstract void OnActive();
+
+    protected abstract void OnRecovery();
+
 }
